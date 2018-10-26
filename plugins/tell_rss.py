97c72ebbff43362327c81a466ac0ace3650c40a0
watanabelab-PC\watanabelab <takuyam0111@yahoo.co.jp>
2018-10-26 14:02:33 +0900

    arxivからダウンロードしたrssのid(URL)のログをとる機能追加

--- plugins/tell_rss.py
+++ plugins/tell_rss.py
@@ -9,27 +9,45 @@ import re
 import pickle
 import os
 import requests
+import csv
 
-SPAN = 60 * 60 * 24
+SPAN = 60 * 60 * 12
 
 @respond_to('RSS')
 def daily(message):
-    print('I run once initially after a delay, then every 24 hours.')    
-    @bot_routine(SPAN, delay=True)
-    def rss():
-    	# query = "(cat:stat.ML+OR+cat:cs.CV+OR+cs.HC+OR+cs.IR)+AND+((abs:emotion)+OR+(abs:ECG)+OR+(abs:time\ series))"
-    	query = "(cat:stat.ML)"
-    	# get rss
-    	rss = get_rss(query)
-    	for _rss in rss:
-	    	message.send(_rss)	
-    return 
+	# print("telling RSS")
+	@bot_routine(SPAN, delay=True)
+	def rss():
+		# print("getting RSS..")
+		# query = "(cat:stat.ML+OR+cat:cs.CV+OR+cs.HC+OR+cs.IR)+AND+((abs:emotion)+OR+(abs:ECG)+OR+(abs:time\ series))"
+		query = "((cat:stat.ML)+OR+(cat:cs.CV)+OR+(cat:cs.LG)+OR+(cat:cs.SD))"
+		# get rss
+		rss = get_rss(query)
+		sent_flag = False
+		# read csv
+		f = open('log.csv', 'r', encoding='UTF-8')
+		csv_data = csv.reader(f)
+		for _rss in rss:
+			# csvに載ってないrssをメッセージで送信
+			if not find(_rss[2], csv_data):
+				sent_flag = True
+				rss_message = create_message(_rss)
+				message.send(rss_message)
+		f.close()
+		if sent_flag:		# write csv
+			f = open('log.csv', 'w', newline='')
+			csvWriter = csv.writer(f)
+			for _rss in rss:
+				csvWriter.writerow([_rss[2]])
+			f.close()
+	return 
 
 def get_rss(query, max_results=5):
 	# url of arXiv API
 	# If you want to customize, please change here.
 	# detail is shown here, https://arxiv.org/help/api/user-manual
-	arxiv_url = 'http://export.arxiv.org/api/query?search_query=' + query + '&start=0&max_results=' + str(max_results) + '&sortBy=lastUpdatedDate&sortOrder=descending'
+	# arxiv_url = 'http://export.arxiv.org/api/query?search_query=' + query + '&start=0&max_results=' + str(max_results) + '&sortBy=lastUpdatedDate&sortOrder=descending'
+	arxiv_url = 'http://export.arxiv.org/api/query?search_query=' + query + '&start=0&max_results=' + str(max_results) + '&sortBy=submittedDate&sortOrder=descending'
 	# Get returned value from arXiv API
 	data = requests.get(arxiv_url).text
 	# Parse the returned value
@@ -37,13 +55,26 @@ def get_rss(query, max_results=5):
 	rss = []
 	for i, entry in enumerate(entries):
 		# Parse each entry
-		url = parse(entry, "id")[0]
+		# write in csv file
+		_ = ()
 		title = parse(entry, "title")[0]
-		date = parse(entry, "published")[0]
 		summary = parse(entry, "summary")[0]
-		_rss = "\n".join(["=" * 10, "No." + str(i + 1), "Title:  " + title, "Summary: " + summary, "URL: " + url, "Published: " + date])
-		rss.append(_rss)
+		url = parse(entry, "id")[0]
+		date = parse(entry, "published")[0]
+		_ = _ + (title, summary, url, date,)
+		rss.append(_)
 	return rss
+
+def find(id, csv_data):
+	for row in csv_data:
+		if row:
+			if id == row[0]:
+				return True
+	return False
+
+def create_message(rss):
+	message = "\n".join(["=" * 10, "Title: " + rss[0], "Summary: " + rss[1], "URL: " + rss[2], "Published: " + rss[3]])
+	return message
 
 def parse(data, tag):
 
